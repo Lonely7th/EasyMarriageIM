8ded73f697b96d308446cb7a5d7a0e4ff3cf1a80
Lonely7th <35439521+Lonely7th@users.noreply.github.com>
2019-03-13 23:49:03 +0800

    2019-03-13

--- app/src/main/java/com/em/im/callback/OnAlterDialogListener.java
+++ app/src/main/java/com/em/im/callback/OnAlterDialogListener.java
@@ -3,7 +3,7 @@ package com.em.im.callback;
 /**
  * Time ： 2018/12/19 0019 .
  * Author ： JN Zhang .
- * Description ： .
+ * Description ：提示框点击事件 .
  */
 public interface OnAlterDialogListener {
     void onRightClick();
